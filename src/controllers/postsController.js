6032491f5972fc19cb0a2bce2f99eb1fee3ec521
btwbeatriz <beatriz.ffreitas@hotmail.com>
2024-11-22 18:10:25 -0300

    implementando rrmazenamento e upload de imagens

--- src/controllers/postsController.js
+++ src/controllers/postsController.js
@@ -1,19 +1,53 @@
+import fs from "fs";
 import { getAllPosts, createPost } from "../models/postsModel.js";
 
+// Função assíncrona para listar todos os posts
 export async function listAllPosts(req, res) {
     // Chama a função para obter todos os posts do banco de dados
     const posts = await getAllPosts();
     // Envia uma resposta HTTP com status 200 e os posts em formato JSON
     res.status(200).json(posts);
-}
-
+  }
+  
+// Função assíncrona para criar um novo post
 export async function postNewPost(req, res) {
+    // Obtém os dados do novo post do corpo da requisição
     const newPost = req.body;
-    try{
-        const createdPost = await createPost(newPost);
-        res.status(200).json(createdPost);
-    }catch(erro){
-        console.error(erro.message);
-        res.status(500).json({"Erro":"Falha na requisição!"});
+    try {
+      // Chama a função para criar o novo post no banco de dados
+      const createdPost = await createPost(newPost);
+      // Envia uma resposta HTTP com status 200 e o post criado em formato JSON
+      res.status(200).json(createdPost);
+    } catch (error) {
+      // Registra o erro no console
+      console.error(error.message);
+      // Envia uma resposta HTTP com status 500 e uma mensagem de erro
+      res.status(500).json({ "Erro": "Falha na requisição!" });
     }
-}
+  }
+  
+// Função assíncrona para fazer upload de uma imagem e criar um novo post
+export async function uploadImage(req, res) {
+    // Cria um objeto com os dados do novo post, incluindo o nome do arquivo da imagem
+    const newPost = {
+      descricao: "",
+      imgUrl: req.file.originalname,
+      alt: ""
+    };
+  
+    try {
+      // Chama a função para criar o novo post no banco de dados
+      const createdPost = await createPost(newPost);
+      // Renomeia o arquivo da imagem para incluir o ID do post criado
+      const updatedImage = `uploads/${createdPost.insertedId}.png`;
+      fs.renameSync(req.file.path, updatedImage);
+      // Envia uma resposta HTTP com status 200 e o post criado em formato JSON
+      res.status(200).json(createdPost);
+    } catch (error) {
+      // Registra o erro no console
+      console.error(error.message);
+      // Envia uma resposta HTTP com status 500 e uma mensagem de erro
+      res.status(500).json({ "Erro": "Falha na requisição!" });
+    }
+  }
+  
--- src/routes/postsRouts.js
+++ src/routes/postsRouts.js
@@ -1,13 +1,36 @@
 import express from "express";
-import { listAllPosts, postNewPost } from "../controllers/postsController.js";
+import multer from "multer";
+import { listAllPosts, postNewPost, uploadImage } from "../controllers/postsController.js";
 
-const routes = (app) => {
-    // Habilita o middleware para analisar requisições com corpo em formato JSON
+// Configura o armazenamento para o multer
+const storage = multer.diskStorage({
+    // Define o diretório de destino para arquivos enviados (./uploads neste caso)
+    destination: function (req, file, cb) {
+      cb(null, 'uploads/');
+    },
+    // Define o nome do arquivo enviado (usa o nome original)
+    filename: function (req, file, cb) {
+      cb(null, file.originalname);
+    }
+  });
+  
+  // Cria uma instância do multer com o armazenamento configurado
+  const upload = multer({ dest: "./uploads", storage });
+  
+  // Define uma função para registrar rotas em uma aplicação Express
+  const routes = (app) => {
+    // Habilita o middleware para interpretar dados JSON no corpo da requisição
     app.use(express.json());
-    // Rota para obter todos os posts
+  
+    // Define uma rota para requisições GET em "/posts" (manipulada pela função listAllPosts)
     app.get("/posts", listAllPosts);
-    // Rota para criar um novo post
+  
+    // Define uma rota para requisições POST em "/posts" (manipulada pela função postNewPost)
     app.post("/posts", postNewPost);
-}
-
-export default routes;
+  
+    // Define uma rota para requisições POST em "/upload" com um único arquivo chamado "image" (manipulada pelo middleware upload.single e depois pela função uploadImage)
+    app.post("/upload", upload.single("image"), uploadImage);
+  };
+  
+  // Exporta a função routes como padrão para uso em outros módulos
+  export default routes;
